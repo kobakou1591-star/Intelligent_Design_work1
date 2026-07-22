79cdf6259997239f2a8a3df7c727c8eb6973e729
kobakou1591-star <kobakou1591@gmail.com>
2026-07-22 13:31:19 +0900

    my_AEを追加

--- scripts/my_AE.py
+++ scripts/my_AE.py
@@ -11,7 +11,7 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(PROJECT_ROOT))
 
 from src.my_dataset import MRRDataset
-from src.my_processing import LogStandardScaler, MRRScaler
+from src.my_preprocessing import LogStandardScaler, MRRScaler
 from src.model import Autoencoder
 from src.my_plot import plot_mrr_comparison
 
